eab45769e1b7d470023aa57c604cca8ca868313b
Lucas Borges <lucasbfernandes94@gmail.com>
2018-12-18 13:46:24 -0200

    Calculating mbps variance and deviation

--- probability-qos/probability-qos.py
+++ probability-qos/probability-qos.py
@@ -38,7 +38,7 @@ def move_result_log(index, distribution):
 def build_deviation_results_row(deviation_results_dict, column_names):
     deviation_results_row = []
     for column_name in column_names:
-        if not column_name == 'drop_rate':
+        if not column_name == 'drop_rate' and not column_name == 'total_flow_mbps' and not column_name == 'total_passed_mbps':
             deviation_results_row.append(int(math.floor(deviation_results_dict[column_name])))
         else:
             deviation_results_row.append(deviation_results_dict[column_name])
@@ -47,7 +47,7 @@ def build_deviation_results_row(deviation_results_dict, column_names):
 
 def get_deviation_results_array(deviation_results):
     deviation_results_array = []
-    column_names = ['max_flow', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed']
+    column_names = ['max_flow', 'total_flow_mbps', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed_mbps', 'total_passed']
     for key in deviation_results:
         deviation_results_array.append([int(key)] + build_deviation_results_row(deviation_results[key], column_names))
     return deviation_results_array
@@ -56,10 +56,12 @@ def get_deviation_results_array(deviation_results):
 def get_deviation_row_data_dict(row):
     return {
         'max_flow': math.sqrt(row['max_flow']),
+        'total_flow_mbps': math.sqrt(row['total_flow_mbps']),
         'total_flow': math.sqrt(row['total_flow']),
         'drop_rate': math.sqrt(row['drop_rate']),
         'packets_dropped': math.sqrt(row['packets_dropped']),
         'total_dropped': math.sqrt(row['total_dropped']),
+        'total_passed_mbps': math.sqrt(row['total_passed_mbps']),
         'total_passed': math.sqrt(row['total_passed'])
     }
 
@@ -72,10 +74,12 @@ def compute_deviation_results(df, deviation_results):
 def compute_variance_row(row_dict, variance_results, key):
     return {
         'max_flow': variance_results[key]['max_flow'] + row_dict['max_flow'],
+        'total_flow_mbps': variance_results[key]['total_flow_mbps'] + row_dict['total_flow_mbps'],
         'total_flow': variance_results[key]['total_flow'] + row_dict['total_flow'],
         'drop_rate': variance_results[key]['drop_rate'] + row_dict['drop_rate'],
         'packets_dropped': variance_results[key]['packets_dropped'] + row_dict['packets_dropped'],
         'total_dropped': variance_results[key]['total_dropped'] + row_dict['total_dropped'],
+        'total_passed_mbps': variance_results[key]['total_passed_mbps'] + row_dict['total_passed_mbps'],
         'total_passed': variance_results[key]['total_passed'] + row_dict['total_passed']
     }
 
@@ -84,10 +88,12 @@ def get_variance_row_data_dict(row, mean_results):
     row_second = row['seconds']
     return {
         'max_flow': math.pow(mean_results[row_second]['max_flow'] - row['max_flow'], 2),
+        'total_flow_mbps': math.pow(mean_results[row_second]['total_flow_mbps'] - (row['total_flow'] * MBPS_MULTIPLIER), 2),
         'total_flow': math.pow(mean_results[row_second]['total_flow'] - row['total_flow'], 2),
         'drop_rate': math.pow(mean_results[row_second]['drop_rate'] - row['drop_rate'], 2),
         'packets_dropped': math.pow(mean_results[row_second]['packets_dropped'] - row['packets_dropped'], 2),
         'total_dropped': math.pow(mean_results[row_second]['total_dropped'] - row['total_dropped'], 2),
+        'total_passed_mbps': math.pow(mean_results[row_second]['total_passed_mbps'] - (row['total_passed'] * MBPS_MULTIPLIER), 2),
         'total_passed': math.pow(mean_results[row_second]['total_passed'] - row['total_passed'], 2)
     }
 
@@ -108,10 +114,12 @@ def compute_variance_results(df, variance_results, mean_results):
 def compute_variance_row_mean(variance_results, row_count, key):
     return {
         'max_flow': variance_results[key]['max_flow'] / row_count[key],
+        'total_flow_mbps': variance_results[key]['total_flow_mbps'] / row_count[key],
         'total_flow': variance_results[key]['total_flow'] / row_count[key],
         'drop_rate': variance_results[key]['drop_rate'] / row_count[key],
         'packets_dropped': variance_results[key]['packets_dropped'] / row_count[key],
         'total_dropped': variance_results[key]['total_dropped'] / row_count[key],
+        'total_passed_mbps': variance_results[key]['total_passed_mbps'] / row_count[key],
         'total_passed': variance_results[key]['total_passed'] / row_count[key]
     }
 
@@ -124,7 +132,7 @@ def compute_variance_results_mean(variance_results, row_count):
 def build_variance_results_row(variance_results_dict, column_names):
     variance_results_row = []
     for column_name in column_names:
-        if not column_name == 'drop_rate':
+        if not column_name == 'drop_rate' and not column_name == 'total_flow_mbps' and not column_name == 'total_passed_mbps':
             variance_results_row.append(int(math.floor(variance_results_dict[column_name])))
         else:
             variance_results_row.append(variance_results_dict[column_name])
@@ -133,7 +141,7 @@ def build_variance_results_row(variance_results_dict, column_names):
 
 def get_variance_results_array(variance_results):
     variance_results_array = []
-    column_names = ['max_flow', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed']
+    column_names = ['max_flow', 'total_flow_mbps', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed_mbps', 'total_passed']
     for key in variance_results:
         variance_results_array.append([int(key)] + build_variance_results_row(variance_results[key], column_names))
     return variance_results_array
@@ -257,12 +265,12 @@ def run_distributions_tests(distributions, number_of_tests):
 
 def generate_deviation_results(distribution, deviation_results):
     df = pandas.read_csv('{path}/{d}/variance_results.log'.format(path=PEXPECT_PROJECT_PATH, d=distribution), sep=';', header=None)
-    df.columns = ['seconds', 'max_flow', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed']
+    df.columns = ['seconds', 'max_flow', 'total_flow_mbps', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed_mbps', 'total_passed']
     compute_deviation_results(df, deviation_results)
     deviation_results_array = get_deviation_results_array(deviation_results)
 
     deviation_df = pandas.DataFrame(deviation_results_array)
-    deviation_df.columns = ['seconds', 'max_flow', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed']
+    deviation_df.columns = ['seconds', 'max_flow', 'total_flow_mbps', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed_mbps', 'total_passed']
     deviation_df.to_csv('{path}/{d}/deviation_results.log'.format(path=PEXPECT_PROJECT_PATH, d=distribution), index=False, sep=';', header=None)
 
 
@@ -276,7 +284,7 @@ def generate_variance_results(number_of_tests, distribution, variance_results, m
     variance_results_array = get_variance_results_array(variance_results)
 
     variance_df = pandas.DataFrame(variance_results_array)
-    variance_df.columns = ['seconds', 'max_flow', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped','total_passed']
+    variance_df.columns = ['seconds', 'max_flow', 'total_flow_mbps', 'total_flow', 'drop_rate', 'packets_dropped', 'total_dropped', 'total_passed_mbps', 'total_passed']
     variance_df.to_csv('{path}/{d}/variance_results.log'.format(path=PEXPECT_PROJECT_PATH, d=distribution), index=False, sep=';', header=None)
 
 
